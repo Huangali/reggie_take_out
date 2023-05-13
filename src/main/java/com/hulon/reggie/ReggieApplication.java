7511a2ea33d9619345bd13f336677f00ef90be54
hulon <1310592852@qq.com>
2023-05-13 16:32:25 +0800

    第一次提交

--- src/main/java/com/hulon/reggie/ReggieApplication.java
+++ src/main/java/com/hulon/reggie/ReggieApplication.java
@@ -1,6 +1,19 @@
-package com.hulon.reggie;/**
+package com.hulon.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * @author Hulon
  * @date 2023/5/13
  * @className ReggieApplication
- */public class ReggieApplication {
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功");
+    }
 }
